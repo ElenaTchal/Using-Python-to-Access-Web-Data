87e42cc39c019cd0c0e7c2220386891aa4979157
Elena Tchalakov <71348324+ElenaTchal@users.noreply.github.com>
2020-10-23 17:13:22 -0700

    Add files via upload

---
+++ xmlextraction.py
@@ -0,0 +1,22 @@
+import urllib.request, urllib.error
+import xml.etree.ElementTree as ET
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+while True:
+    address = "http://py4e-data.dr-chuck.net/comments_980730.xml"
+    if len(address) < 1: break
+
+    uh = urllib.request.urlopen(address, context=ctx)
+    data = uh.read()
+    tree = ET.fromstring(data)
+    counts = tree.findall('.//count')
+    counts_strings = [count.text for count in counts]
+    counts_integers = [int(i) for i in counts_strings]
+
+print("Sum: ", sum(counts_integers))
+
